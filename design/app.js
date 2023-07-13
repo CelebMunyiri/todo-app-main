6d88c01aac36944435212840e4bf775135a2d168
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-13 18:02:53 +0300

    mplemented all logics

--- design/app.js
+++ design/app.js
@@ -1,35 +1,100 @@
 "use strict"
-let sendBtn=document.querySelector(`.kadot`);
 let checkSvg=document.querySelector('.checkSvg');
 let inputValue=document.querySelector('.input');
 let tasksClass=document.querySelector('.lower');
-let completed=document.querySelector('.completed');
+let complete=document.querySelector('.complete');
 let clearCompleted=document.querySelector('.clearCompleted');
 let all=document.querySelector('.all');
+const active=document.querySelector('.active')
 let tasks=[]
 
 
-sendBtn.addEventListener(`click`,()=>{
-    sendBtn.style.background="linear-gradient(to bottom,hsl(192, 100%, 67%), hsl(280, 87%, 65%))";
-checkSvg.style.opacity=1;
-    console.log('clicked')
-
-})
 document.addEventListener('submit',(e)=>{
-    e.preventDefault();
-console.log(inputValue.value);
-todo.push(inputValue.value)
-// let todoItems=todo.push(inputValue.value);
-let task=document.createElement('div');
-tasksClass.appendChild(task)
-
-let parag=document.createElement('p')
-for(let i=0;i<=todo.length;i++)
-parag.innerText=todo[i]
-task.appendChild(parag);
-
-
-
+  e.preventDefault();
+addTask();
 })
-console.log(todo);
 
+
+
+function addTask() {
+  const taskInput = document.getElementById('taskInput');
+  const taskText = taskInput.value.trim();
+  if (taskText !== '') {
+    const task = {
+      text: taskText,
+      completed: document.getElementById('taskCheckbox').checked
+    };
+    tasks.push(task);
+    taskInput.value = '';
+    document.getElementById('taskCheckbox').checked = false;
+    showAllTasks();
+  }
+}
+
+function showAllTasks() {
+  const taskList = document.getElementById('taskList');
+  taskList.innerHTML = '';
+  tasks.forEach((task, index) => {
+    const li = document.createElement('li');
+    if (task.completed) {
+      li.classList.add('completed');
+    }
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.completed;
+    checkbox.disabled = false; // Enable the checkbox
+    checkbox.addEventListener('change', () => toggleTaskCompletion(index)); // Add event listener
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(task.text));
+    taskList.appendChild(li);
+  });
+}
+
+
+function showCompletedTasks() {
+  const taskList = document.getElementById('taskList');
+  taskList.innerHTML = '';
+  tasks.filter(task => task.completed).forEach(task => {
+    const li = document.createElement('li');
+    li.classList.add('completed');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = true;
+    checkbox.disabled = true;
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(task.text));
+    taskList.appendChild(li);
+  });
+}
+
+function showActiveTasks() {
+  const taskList = document.getElementById('taskList');
+  taskList.innerHTML = '';
+  tasks.filter(task => !task.completed).forEach((task, index) => {
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = false;
+    checkbox.disabled = false; // Enable the checkbox
+    checkbox.addEventListener('change', () => toggleTaskCompletion(index)); // Add event listener
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(task.text));
+    taskList.appendChild(li);
+  });
+}
+
+function clearCompletedTasks() {
+  tasks = tasks.filter(task => !task.completed);
+  showAllTasks();
+}
+
+function toggleTaskCompletion(index) {
+  tasks[index].completed = !tasks[index].completed;
+  showAllTasks();
+}
+
+all.addEventListener('click',()=>showAllTasks());
+active.addEventListener('click',()=>showActiveTasks());
+complete.addEventListener('click',()=>showCompletedTasks());
+clearCompleted.addEventListener('click',()=>clearCompletedTasks());
+ 
